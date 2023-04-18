4744aa2a08167dc07067bdffa9491b49bbc47b23
vivienwu7 <wuvivien277@gmail.com>
2023-04-18 06:13:33 -0500

    Initial Commit

---
+++ LoginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Vivien Wu on 4/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
